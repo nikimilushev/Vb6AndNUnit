728aa456f7e36c5e50c1945f905a66f5f62361ef
Nikolay Milushev <nikolay.milushev@fourth.com>
2017-02-06 11:22:05 +0200

    first commit

---
+++ NUnitCOMTest/NUnitCOMTest/COMUnitTest.cs
@@ -0,0 +1,32 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NUnitCOMTest
+{
+    [TestFixture]
+    public class COMUnitTest
+    {
+        [STAThread]
+        [Test]
+        public void TestCreate_COMObjectSucceeds()
+        {
+            dynamic comObject = CreateComComponent("COMUnitTest.ClassToTest");
+
+            var message = comObject.MethodToTest();
+
+            Assert.AreEqual("Hello from ClassToTest", message); 
+
+        }
+        private dynamic CreateComComponent(string progId) 
+        {
+            Type type = Type.GetTypeFromProgID(progId);
+            if (type == null)
+            {
+                throw new InvalidOperationException(
+                    string.Format("The {0} is not registered on this system.", progId));
+            }
+
+            return Activator.CreateInstance(type) as dynamic;
+        }
+    }
+}
